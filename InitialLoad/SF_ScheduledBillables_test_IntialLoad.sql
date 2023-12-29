e9061c503d43735c394ef34b2f0fbad44f7059a2
IanLockDakota <ilockwood@dakotafin.com>
2023-12-29 15:33:48 -0800

    accounting, schbills, pymt, invoicedetail changes

--- InitialLoad/SF_ScheduledBillables_test_IntialLoad.sql
+++ InitialLoad/SF_ScheduledBillables_test_IntialLoad.sql
@@ -38,6 +38,7 @@ USING (SELECT
 	PS.PaymentStreamOID AS PaymentStreamOID__c,
 	PS.StartDate AS StartDate__c,
 	PS.Occurrences AS Occurrences__c,
+    /*HOW MANY REMAINING - uninvoiced*/
 	PS.Frequency AS Frequency__c,
 	PS.isInvoiced AS isInvoiced__c,
     PSCI.PaymentStreamContractItemOID AS PaymentStreamContractItemOID__c,
@@ -45,8 +46,8 @@ USING (SELECT
 	PSCI.OnStreamTaxAmount AS OnStreamTaxAmount__c,
 	PSCI.Amount + PSCI.OnStreamTaxAmount AS TtlAmt__C,
 	CASE
-		WHEN ps.IsInvoiced = 0 THEN ps.StartDate
-		WHEN ps.IsInvoiced = 1 THEN NULL
+		WHEN (c.isTerminated = 1) THEN NULL
+		WHEN (c.isTerminated = 0) AND (ps.isInvoiced = 0) AND (ps.startDate > GETDATE()) THEN ps.StartDate
 		ELSE NULL
 	END as NextPaymentDate__c,
 	PS.LastChangeDateTime AS LastChangeDateTime__c,
@@ -76,6 +77,7 @@ USING (SELECT
 
 WHEN MATCHED THEN
     UPDATE SET
+        Target.Opportunity__c = Source.Opportunity__c,
         Target.ScheduleDefinitionOID__c = Source.ScheduleDefinitionOID__c,
         Target.RecurringBillableOid__c = Source.RecurringBillableOid__c,
         Target.IsFollowingRent__c = Source.IsFollowingRent__c,
@@ -108,6 +110,7 @@ WHEN NOT MATCHED THEN
         IsBillAfterTermination__c,
         TCTransCDesc__C,
         TransCDesc__c,
+        PaymentStreamOID__c,
         StartDate__c,
         Occurrences__c,
         Frequency__c,
@@ -123,7 +126,7 @@ WHEN NOT MATCHED THEN
     ) VALUES (
         source.ID,
         source.Opportunity__c,
-        ScheduleDefinitionOID__c,
+        source.ScheduleDefinitionOID__c,
         source.RecurringBillableOid__c,
         source.IsFollowingRent__c,
         source.IsCombinedWithRent__c,
@@ -131,6 +134,7 @@ WHEN NOT MATCHED THEN
         source.IsBillAfterTermination__c,
         source.TCTransCDesc__C,
         source.TransCDesc__c,
+        source.PaymentStreamOID__c,
         source.StartDate__c,
         source.Occurrences__c,
         source.Frequency__c,
